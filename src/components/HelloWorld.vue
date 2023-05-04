acdb5e52b93abfd0713aca83f95e7eb6a195443f
k321m <90863488+k321m@users.noreply.github.com>
2023-05-05 02:06:03 +0900

    add: 3Dアニメーション

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from "vue";
+import { useHead } from "@vueuse/head";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p>HelloWorld</p>
+  <div id="background"></div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script>
+import NET from "vanta/dist/vanta.net.min";
+export default {
+  mounted() {
+    this.vantaEffect = NET({
+      el: "#background",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 500.0,
+      minWidth: 500.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0xffffff,
+    });
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createHead } from "@vueuse/head";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const head = createHead();
+
+app.use(head);
+app.mount("#app");
