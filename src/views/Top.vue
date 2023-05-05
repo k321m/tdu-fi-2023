843c864e8fb23674e0306d09ff13eab044e2e266
k321m <90863488+k321m@users.noreply.github.com>
2023-05-05 21:43:57 +0900

    add: カルーセル

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>
-    <p>Topページです</p>
-  </div>
+  <p>Topページです.</p>
+  <Carousel
+    :itemsToShow="1.5"
+    :wrapAround="true"
+    :transition="1500"
+    :autoplay="3500"
+  >
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+  </Carousel>
 </template>
 
 <script>
+import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 export default {
   name: "Top",
+  components: {
+    Carousel,
+    Slide,
+  },
 };
 </script>
+
+<style scoped>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 1.5s;
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
+}
+</style>
