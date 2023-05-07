05987ba0a4dd1017cc033c6dc28ceea54e2826fe
k321m <90863488+k321m@users.noreply.github.com>
2023-05-07 13:20:34 +0900

    fix: 星

--- src/js/particles/top-particle.js
+++ src/js/particles/top-particle.js
@@ -3,129 +3,65 @@ window.addEventListener("load", function () {
   particlesJS("particles-js", {
     //パーティクルの設定
     particles: {
-      //シェイプの数
-      number: {
-        value: 20, //シェイプの数
-        density: {
-          enable: true, //密度を変更する
-          value_area: 1000, //密集度
-        },
-      },
-      //色
-      color: {
-        value: ["26B7BC", "008FB3", "FAF44B", "5B98D2", "4DE2D6"],
-      },
-      //シェイプの形状
+      number: { value: 72, density: { enable: true, value_area: 400 } },
+      color: { value: "#a571d9" },
       shape: {
-        type: "circle", //シェイプの形(円)
-        stroke: {
-          width: 0, //外線つけない
-          color: "#000000", //つける場合の線色
-        },
-        //※typeがpolygon(多角形)の場合
-        polygon: {
-          nb_sides: 5, //角の数
-        },
-        //typeがimageの場合
-        image: {
-          src: "img/gazou.png", //画像の指定
-          width: 100, //画像の幅
-          height: 100, //画像の高さ
-        },
+        type: "star",
+        stroke: { width: 1, color: "#e345e6" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 },
       },
-      //シェイプの透明度
       opacity: {
-        value: 1, //透明度(1で不透明)
-        random: false, //透明度をランダムにしない
-        //randomがtrueの場合
-        anim: {
-          enable: false, //透明度のアニメーションをしない
-          speed: 1, //アニメーションスピード
-          opacity_min: 0.1, //透明度の最小値
-          sync: false, //各シェイプを同時に動かさない
-        },
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
       },
-      //シェイプの大きさ
       size: {
-        value: 80, //大きさ
-        random: true, //大きさをランダムにする falseで均一
+        value: 2,
+        random: true,
         anim: {
-          enable: false, //シェイプの大きさをアニメーションしない
-          speed: 10, //アニメーションのスピード
-          size_min: 0.1, //サイズの最小値
-          sync: false, //各シェイプを同時に動かさない
+          enable: true,
+          speed: 1,
+          size_min: 1.6240621041348633,
+          sync: false,
         },
       },
-      //シェイプ間を結ぶ線
       line_linked: {
-        enable_auto: false, //線をつけない
-        distance: 0, //繋がりの数
-        color: "#000", //線の色
-        opacity: 1, //透明度　1で不透明
-        width: 1, //線の太さ
+        enable: true,
+        distance: 150,
+        color: "#00d1ff",
+        opacity: 0.4,
+        width: 1,
       },
-      //シェイプの動き
       move: {
-        enable: true, //動きを付ける
-        speed: 2, //シェイプが動くスピード数値が大きいと早い
-        direction: "none", //動きの方向(none、top、top-right、right、bottom-right、bottom、bottom-left、left、top-left)
-        straight: false, //動きを動きを止めるか否か
-        out_mode: "bounce", //外枠に達した際のシェイプの動き　bounce→跳ね返る　out→フレームアウト
-        /*シェイプを引き寄せる*/
-        attract: {
-          enable: false, //引き寄せない
-          rotateX: 600, //横軸の指定
-          rotateY: 1200, //横軸の指定
-        },
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
       },
     },
-    //相互作用
     interactivity: {
       detect_on: "canvas",
       events: {
-        //カーソルを乗せた時の動き
-        onhover: {
-          enable: false, //無効
-          mode: "repulse", //enableがtrueの場合の動き(下の★のリストの名前入れるとその動きが発動)
-        },
-        //クリックしたときの動き
-        onclick: {
-          enable: false, //無効
-          mode: "push", //(下の★のリストの名前入れるとその動きが発動)
-        },
-        resize: true, //リサイズしたとき拡縮しない
+        onhover: { enable: false, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true,
       },
-      //各モード設定した場合の動き
       modes: {
-        //★シェイプとカーソルの間に線ができる
         grab: {
-          distance: 400, //カーソルからの反応距離
-          line_linked: {
-            opacity: 1, //線の不当明度
-          },
+          distance: 611.3886113886114,
+          line_linked: { opacity: 0.6960913660388353 },
         },
-        //★シェイプが膨らむ
-        bubble: {
-          distance: 200, //カーソルからの反応距離
-          size: 40, //膨らむ際の大きさ
-          duration: 2, //持続時間
-          opacity: 8, //透明度
-          speed: 3, //速度
-        },
-        //★シェイプに触れるとカーソルから逃げる
-        repulse: {
-          distance: 200, //カーソルからの反応距離
-        },
-        //★シェイプを増やす
-        push: {
-          particles_nb: 4, //数
-        },
-        //★シェイプを減らす
-        remove: {
-          particles_nb: 2, //数
-        },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
       },
     },
-    retina_detect: true, //Retina Display(*高画素密度のディスプレイ)を対応する
+    retina_detect: true,
   });
 });
