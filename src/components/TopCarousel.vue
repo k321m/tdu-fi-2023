ff0120fd5d4850e332ae1cc80769f8bf1e1d27b8
k321m <90863488+k321m@users.noreply.github.com>
2023-05-09 11:52:45 +0900

    add: カルーセル文字入れ

--- src/components/TopCarousel.vue
+++ src/components/TopCarousel.vue
@@ -7,8 +7,13 @@
       :transition="2500"
       :autoplay="5000"
     >
-      <Slide v-for="slide in 10" :key="slide">
-        <div class="carousel__item">{{ slide }}</div>
+      <Slide v-for="slide in contents" :key="slide">
+        <div class="carousel__item">
+          <div class="text-box pa-2">
+            <p class="jp-text">{{ slide.jptext }}</p>
+            <p class="text py-1">{{ slide.text }}</p>
+          </div>
+        </div>
       </Slide>
       <template #addons>
         <Pagination />
@@ -22,6 +27,16 @@ import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 export default {
   name: "TopCarousel",
+  data() {
+    return {
+      contents: [
+        {
+          text: "Laboratory",
+          jptext: "研究室展示",
+        },
+      ],
+    };
+  },
   components: {
     Carousel,
     Slide,
@@ -31,6 +46,28 @@ export default {
 </script>
 
 <style scoped>
+.text-box {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  text-align: right;
+}
+p.jp-text {
+  font-family: zen-kaku-gothic-new, sans-serif;
+  font-weight: 700;
+  font-size: 0.7em;
+  line-height: 1;
+  font-style: normal;
+}
+p.text {
+  font-family: hack, sans-serif;
+  font-weight: 700;
+  font-size: 1.5em;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: -0.03em;
+}
+
 .carousel__item {
   min-height: 220px;
   width: 100%;
