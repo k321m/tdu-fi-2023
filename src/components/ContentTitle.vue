70870fd2a89c749d721e5ba9f13c01a83e528de2
k321m <90863488+k321m@users.noreply.github.com>
2023-05-19 14:46:42 +0900

    delete: いらないもの削除

--- src/components/ContentTitle.vue
+++ src/components/ContentTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="contents">
+  <div>
     <div class="py-4">
       <p class="zen-kaku-bold pl-4 py-1"><slot name="jp-title"></slot></p>
       <p class="hack-h2 py-1"><slot name="title"></slot></p>
--- src/views/MyNoteView.vue
+++ src/views/MyNoteView.vue
@@ -5,38 +5,11 @@
         <template v-slot:jp-title>マイノート</template>
         <template v-slot:title>>MyNote</template>
         <template v-slot:info
-          >説明説明説明説明説明説明説明説明説説明説明説明説
-          明説明説明説明説明説明説明説明説明説明説明説明説
-          明説明説明説明説明説明説明説明説明説明説明説説明
+          >説明説明説明説明説明説明説明説明説説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説説明
         </template>
       </ContentTitle>
       <div class="pt-6">
         <p class="zen-kaku-bold">訪問リスト</p>
-        <!-- <div>
-          <v-expansion-panels>
-            <v-expansion-panel
-              class="my-1"
-              v-for="data in myNoteData"
-              :key="data"
-            >
-              <v-expansion-panel-title>
-                {{ data.title }}
-              </v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <div>
-                  <p>
-                    {{ data.event }}
-                  </p>
-                  <p>
-                    <span>{{ data.place }}</span>
-                  </p>
-                  <v-btn>地図を確認</v-btn>
-                  <v-btn>リストから削除</v-btn>
-                </div>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </div> -->
         <div v-for="data in myNoteData" :key="data">
           <MyNoteAcordion :id="data.id">
             <template v-slot:title>{{ data.title }}</template>
