c9d24b9642762752fd5a81d2db6206fea7f2a011
直原寿々花 <104682784+jdsuzurec@users.noreply.github.com>
2023-05-23 00:41:41 +0900

    add:タイトル

--- src/views/LabsView.vue
+++ src/views/LabsView.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="contents">
-    <p>Labsページです</p>
+    <div class="pa-4">
+      <ContentTitle>
+        <template v-slot:jp-title>研究室公開</template>
+        <template v-slot:title>>Laboratory</template>
+        <template v-slot:info
+          >説明説明説明説明説明説明説明説明説説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説説明
+        </template>
+      </ContentTitle>
+      <div class="pt-6">
+        <p>Labsページです</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ContentTitle from "../components/ContentTitle.vue";
 export default {
   name: "Labs",
+  components: {
+    ContentTitle,
+  },
 };
 </script>
