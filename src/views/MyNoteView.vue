88a63a1b35af76b5ea4bac29cd9162187056203b
直原寿々花 <104682784+jdsuzurec@users.noreply.github.com>
2023-06-06 17:05:41 +0900

    change: pdfmake method

--- src/views/MyNoteView.vue
+++ src/views/MyNoteView.vue
@@ -70,27 +70,49 @@ export default {
     },
   },
   methods: {
-    addContent(content, title, data) {
+    //タイトルをコンテンツに追加
+    addTitleToContent(content, title) {
       content.push({
         text: title,
         fontSize: 20,
         bold: true,
         margin: [0, 16, 0, 8],
       });
+      return content;
+    },
+    //項目とメモをコンテンツに追加
+    addMemoToContent(content, data) {
       Object.entries(data).forEach(([key, value]) => {
-        content.push({
-          text: "・" + value.title,
-          fontSize: 14,
-          bold: true,
-        });
-        if (value.memo != "") {
-          content.push({
-            text: value.memo,
-            fontSize: 12,
-            margin: [12, 2, 0, 6],
-          });
-        } else {
-          content.push({ text: "-", fontSize: 12, margin: [12, 2, 0, 6] });
+        switch (key) {
+          // なんでもメモの場合は.memoを追加
+          case "memo":
+            if (data.memo != "") {
+              content.push({
+                text: data.memo,
+                fontSize: 12,
+                marginBottom: 6,
+              });
+            } else {
+              content.push({ text: "-", fontSize: 12, marginBottom: 6 });
+            }
+            break;
+          default:
+            // それ以外は項目名とメモを追加
+            content.push({
+              text: "・" + value.title,
+              fontSize: 14,
+              bold: true,
+            });
+            if (value.memo != "") {
+              content.push({
+                text: value.memo,
+                fontSize: 12,
+                margin: [12, 2, 0, 6],
+              });
+            } else {
+              content.push({ text: "-", fontSize: 12, margin: [12, 2, 0, 6] });
+            }
+            break;
         }
       });
       return content;
@@ -106,26 +128,14 @@ export default {
       this.myNoteData = this.$store.getters.getMyNote;
       let content = [];
       // イベントメモ
-      this.addContent(content, "イベントメモ", this.myNoteData.events);
+      this.addTitleToContent(content, "イベントメモ");
+      this.addMemoToContent(content, this.myNoteData.events);
       // Q&Aメモ
-      this.addContent(content, "Q&Aメモ", this.myNoteData.questions);
+      this.addTitleToContent(content, "Q&Aメモ");
+      this.addMemoToContent(content, this.myNoteData.questions);
       // なんでもメモ
-      content.push({
-        text: "なんでもメモ",
-        fontSize: 20,
-        bold: true,
-        margin: [0, 16, 0, 8],
-      });
-      if (this.myNoteData.anything.memo != "") {
-        content.push({
-          text: this.myNoteData.anything.memo,
-          fontSize: 12,
-          marginBottom: 6,
-        });
-      } else {
-        content.push({ text: "-", fontSize: 12, marginBottom: 6 });
-      }
-
+      this.addTitleToContent(content, "なんでもメモ");
+      this.addMemoToContent(content, this.myNoteData.anything);
       var docDefinition = {
         pageSize: "A4", // PDF用紙サイズ設定
         pageMargins: [10, 10, 10, 30], // PDF用紙マージン設定[左、上、右、下]
