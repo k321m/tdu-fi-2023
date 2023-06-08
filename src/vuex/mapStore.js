f32ad0658e47b8e4014dd939480ade1b2e1996a2
直原寿々花 <104682784+jdsuzurec@users.noreply.github.com>
2023-06-08 18:51:17 +0900

    add: map png image

--- src/vuex/mapStore.js
+++ src/vuex/mapStore.js
@@ -2,6 +2,8 @@ import { store } from "./index";
 import map1Img from "/src/assets/maps/Map01.svg";
 import map2Img from "/src/assets/maps/Map02.svg";
 import map3Img from "/src/assets/maps/Map03.svg";
+import map2Png from "/src/assets/maps/Map02.png";
+import map3Png from "/src/assets/maps/Map03.png";
 
 export default {
   namespaced: true,
@@ -17,13 +19,13 @@ export default {
         title: "学科展示室/研究室公開",
         site: "1号館4階 演習室",
         img: "background-image: url(" + map2Img + ")",
-        src: map2Img,
+        src: map2Png,
       },
       map_3: {
         title: "研究室公開",
         site: "1号館5階 メディアラボ",
         img: "background-image: url(" + map3Img + ")",
-        src: map3Img,
+        src: map3Png,
       },
     },
   },
