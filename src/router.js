ea2c664574efb1de8118d223ac5098e168e1eacc
k321m <90863488+k321m@users.noreply.github.com>
2023-06-08 19:09:56 +0900

    add: password

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import MyNote from "./views/MyNoteView.vue";
 import Labs from "./views/LabsView.vue";
 import LectureVideo from "./views/LectureVideoView.vue";
 import Questions from "./views/QuestionsView.vue";
+import Login from "./views/LoginView.vue";
+import { store } from "./vuex/index.js";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -12,26 +14,36 @@ export const router = createRouter({
       path: "/",
       name: "index",
       component: Top,
+      meta: { require: true },
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
     },
     {
       path: "/labs",
       name: "labs",
       component: Labs,
+      meta: { require: true },
     },
     {
       path: "/lecture-video",
       name: "lecture-video",
       component: LectureVideo,
+      meta: { require: true },
     },
     {
       path: "/questions",
       name: "questions",
       component: Questions,
+      meta: { require: true },
     },
     {
       path: "/my-note",
       name: "my-note",
       component: MyNote,
+      meta: { require: true },
     },
   ],
   scrollBehavior(to) {
@@ -41,4 +53,12 @@ export const router = createRouter({
       return { top: 0 };
     }
   },
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.require && store.getters.getPassword != "tdufioc06") {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
