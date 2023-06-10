17658e91a6aa6ae0a66faf106804fc0e76e065c4
k321m <90863488+k321m@users.noreply.github.com>
2023-06-10 14:52:02 +0900

    fix: myNoteへのページ内遷移

--- src/views/AttentionView.vue
+++ src/views/AttentionView.vue
@@ -57,13 +57,11 @@
       </li>
     </ul>
 
-    <p
+    <div
       id="attention-mynote"
-      class="zen-kaku-bold pb-3"
-      style="font-size: 1.2em"
-    >
-      5. MyNote
-    </p>
+      style="padding-top: 72px; margin-top: -72px"
+    ></div>
+    <p class="zen-kaku-bold pb-3" style="font-size: 1.2em">5. MyNote</p>
     <p class="zen-kaku-regular pb-8">
       本ウェブサイトのMyNoteという機能では、登録した項目やメモなどのデータを保存するために「localStorage（ローカルストレージ）」と呼ばれる技術を使用しています。
       ローカルストレージとはWeb
